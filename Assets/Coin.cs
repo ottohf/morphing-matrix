f79685479a327ed0b8f95a6bfa815f15d9cd2353
Otto Fearnley <96774702+ottohf@users.noreply.github.com>
2024-12-04 22:52:02 +0900

    17 persistent coin logic (#20)
    * add persistent coin logic and coin prefab
    
    * coin design and prefabs
    
    * remove coin upon collection
    
    * prefab workaround
    
    * move coins
    
    * remove unused script

---
+++ Assets/Coin.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    [SerializeField]
+    private FloatSO coinSO;
+   
+    bool ObjectIsCoin(GameObject component)
+    {
+        if (component.CompareTag("Coin")){
+            return true;
+        }
+        else
+        {
+            return false;
+        } 
+        
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (ObjectIsCoin(collider.gameObject)) {
+            coinSO.Value += 1;
+            collider.gameObject.SetActive(false);
+        }
+    }
+
+}
