db2088c14365fab9ae1f4a8a9ef09c33b4adb765
darius.engler <darius.engler@epita.fr>
2024-12-04 23:00:00 +0900

    add sound

--- Assets/Scripts/WinScreen.cs
+++ Assets/Scripts/WinScreen.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class WinScreen : MonoBehaviour
 {
+    public void Start()
+    {
+        SoundManager.instance.bgm.clip = SoundManager.instance.winSound;
+        SoundManager.instance.bgm.loop = false;
+        SoundManager.instance.bgm.Play();
+    }
+
     public void GotoMainMenu()
     {
+        SoundManager.instance.bgm.loop = true;
         SceneManager.LoadScene("MainMenu");
     }
 }
