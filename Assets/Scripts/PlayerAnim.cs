1c13d0d59dc6651bd8fed66d514250c01be7b3f3
AIwarning1109 <robinrode2002@gmail.com>
2024-12-04 16:33:16 +0100

    added bowtie for tutorial

--- Assets/Scripts/PlayerAnim.cs
+++ Assets/Scripts/PlayerAnim.cs
@@ -7,6 +7,7 @@ public class PlayerAnim : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody2D rb;
     private Animator animator;
+    private Animator animatorBowtie;
     private bool isGrounded;
 
     public LayerMask groundLayer;
@@ -17,6 +18,7 @@ public class PlayerAnim : MonoBehaviour
     {
         rb = transform.parent.GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        animatorBowtie = GameObject.Find("BowtieAnim").GetComponent<Animator>();
         collider = transform.parent.GetComponent<BoxCollider2D>();
         isGrounded = false;
         Application.targetFrameRate = 60;
@@ -36,10 +38,12 @@ public class PlayerAnim : MonoBehaviour
             if (rb.velocity.x != 0)
             {
                 animator.SetTrigger("penguin_walk_do");
+                animatorBowtie.SetTrigger("bowtie_walk_do");
             }
             else
             {
                 animator.SetTrigger("penguin_idle_do");
+                animatorBowtie.SetTrigger("bowtie_idle_do");
             }
         }
         else
@@ -47,10 +51,12 @@ public class PlayerAnim : MonoBehaviour
             if (rb.velocity.y > 1f)
             {
                 animator.SetTrigger("penguin_up_do");
+                animatorBowtie.SetTrigger("bowtie_up_do");
             }
             else
             {
                 animator.SetTrigger("penguin_down_do");
+                animatorBowtie.SetTrigger("bowtie_down_do");
             }
         }
     }
---
+++ Assets/Scripts/VanityManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VanityManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public bool hasBowtie;
+    public bool hasHat;
+    GameObject bowtie;
+    GameObject hat;
+    GameObject player;
+
+    public SpriteRenderer spriteRendererBowtie;
+    public SpriteRenderer spriteRendererHat;
+    public SpriteRenderer spriteRendererPlayer;
+    void Start()
+    {
+        hasBowtie = false;
+        bowtie = GameObject.Find("BowtieAnim");
+        hat = GameObject.Find("HatAnim");
+        player = GameObject.Find("PlayerSprite");
+        bowtie.transform.position = new Vector2(-100, -100);
+        spriteRendererBowtie = bowtie.GetComponent<SpriteRenderer>();
+        spriteRendererHat = hat.GetComponent<SpriteRenderer>();
+        spriteRendererPlayer = player.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasBowtie)
+        {
+            bowtie.transform.position = player.transform.position;
+            spriteRendererBowtie.transform.localScale = spriteRendererPlayer.transform.localScale;
+        }
+    }
+}
